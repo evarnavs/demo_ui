7af9a2acbb05a648a0336b96a94e1b7a5ea93a85
evarnavs <evarnavs@users.noreply.github.com>
2025-05-06 16:23:14 +0000

    deploy: 3917181eb853ed9e502e43117a5a7e7fe356b87b

--- locators/login_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginLocators:
-    LOGIN_WITH_EMAIL_BTN = (By.XPATH, "//div[@class='btn__content-container' and contains(text(), 'Login with Email/Access Token')]")
-    EMAIL_INPUT = (By.XPATH, "//input[@type='text' and @placeholder='Enter you email or access token']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@type='password' and @placeholder='Enter your password']")
-    LOGIN_BTN = (By.XPATH, "//div[@class='btn__content-container' and text()='Login']")
